6215e13e01c2ecbe992193b38fce0149c49da972
ibadil307 <ibadil307@gmail.com>
2023-12-21 16:55:34 +0800

    Coba

---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+var static = "GoMobileUXPRO Web";
+var cacheassets = [
+    'assets/img/logo.png',
+];
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(static).then(function (cache) {
+            cache.addAll(cacheassets);
+        }).then(function () {
+            return self.skipWaiting();
+        })
+    );
+});
+self.addEventListener("activate", function (event) { });
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request)
+        })
+    );
+});
